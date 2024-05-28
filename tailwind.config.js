462c5fe082b7bf53dd4613a246535d5b5cae27ef
Lucas de Sousa Silva <lucas.sousa07@aluno.ifce.edu.br>
2024-05-28 19:09:07 -0300

    Update Files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
+const { url } = require('inspector');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/*.css'],
   theme: {
     extend: {
       colors: {
-        'Very-Dark-Magenta': 'hsl(300, 43%, 22%)',
-        'Soft-Pink': 'hsl(333, 80%, 67%)',
-        'Dark-Grayish-Magenta': 'hsl(303, 10%, 53%)',
-        'Light-Grayish-Magenta': 'hsl(300, 24%, 96%)',
-        'White': 'hsl(0, 0%, 100%)'
+        'color-Very-Dark-Magenta': 'hsl(300, 43%, 22%)',
+        'color-Soft-Pink': 'hsl(333, 80%, 67%)',
+        'color-Dark-Grayish-Magenta': 'hsl(303, 10%, 53%)',
+        'color-Light-Grayish-Magenta': 'hsl(300, 24%, 96%)',
+        'color-White': 'hsl(0, 0%, 100%)'
       },
       fontFamily: {
-        LeagueSpartan: "League Spartan, sans-serif"
+        'League-Spartan': "League Spartan, sans-serif"
+      },
+      backgroundImage: {
+        'pattern-top-mobile': "url('./images/bg-pattern-top-mobile.svg')",
+        'pattern-bottom-mobile': "url('./images/bg-pattern-bottom-mobile.svg')",
+        'pattern-top-desktop': "url('./images/bg-pattern-top-desktop.svg')",
+        'pattern-bottom-desktop': "url('./images/bg-pattern-bottom-desktop.svg')"
       }
     },
   },
